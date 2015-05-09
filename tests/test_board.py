1b2b4770f98741b0593fa79f204849d6e94609bc
Edmund Jorgensen <tomheon@gmail.com>
2015-05-09 18:44:16 -0400

    Use offense / defense instead of left / right.

--- tests/test_board.py
+++ tests/test_board.py
@@ -7,20 +7,20 @@ YELLOW = ColoredTile('Y')
 YELLOW2 = ColoredTile('Y')
 RED = ColoredTile('R')
 
-YELLOW_STRIKE_123_LEFT = StrikeTile(color='Y', strength=123, direction='<')
-YELLOW_STRIKE_124_LEFT = StrikeTile(color='Y', strength=124, direction='<')
-YELLOW_STRIKE_123_RIGHT = StrikeTile(color='Y', strength=123, direction='>')
-RED_STRIKE_123_LEFT = StrikeTile(color='R', strength=123, direction='<')
+YELLOW_STRIKE_123_OFFENSE = StrikeTile(color='Y', strength=123, direction='<')
+YELLOW_STRIKE_124_OFFENSE = StrikeTile(color='Y', strength=124, direction='<')
+YELLOW_STRIKE_123_DEFENSE = StrikeTile(color='Y', strength=123, direction='>')
+RED_STRIKE_123_OFFENSE = StrikeTile(color='R', strength=123, direction='<')
 
-YELLOW_ATTACK_123_LEFT = AttackTile(color='Y', strength=123, direction='<')
-YELLOW_ATTACK_124_LEFT = AttackTile(color='Y', strength=124, direction='<')
-YELLOW_ATTACK_123_RIGHT = AttackTile(color='Y', strength=123, direction='>')
-RED_ATTACK_123_LEFT = AttackTile(color='R', strength=123, direction='<')
+YELLOW_ATTACK_123_OFFENSE = AttackTile(color='Y', strength=123, direction='<')
+YELLOW_ATTACK_124_OFFENSE = AttackTile(color='Y', strength=124, direction='<')
+YELLOW_ATTACK_123_DEFENSE = AttackTile(color='Y', strength=123, direction='>')
+RED_ATTACK_123_OFFENSE = AttackTile(color='R', strength=123, direction='<')
 
-YELLOW_PROTECT_123_LEFT = ProtectTile(color='Y', strength=123, direction='<')
-YELLOW_PROTECT_124_LEFT = ProtectTile(color='Y', strength=124, direction='<')
-YELLOW_PROTECT_123_RIGHT = ProtectTile(color='Y', strength=123, direction='>')
-RED_PROTECT_123_LEFT = ProtectTile(color='R', strength=123, direction='<')
+YELLOW_PROTECT_123_OFFENSE = ProtectTile(color='Y', strength=123, direction='<')
+YELLOW_PROTECT_124_OFFENSE = ProtectTile(color='Y', strength=124, direction='<')
+YELLOW_PROTECT_123_DEFENSE = ProtectTile(color='Y', strength=123, direction='>')
+RED_PROTECT_123_OFFENSE = ProtectTile(color='R', strength=123, direction='<')
 
 YELLOW_COUNTDOWN_5 = CountdownTile(color='Y', turns_left=5)
 YELLOW_COUNTDOWN_6 = CountdownTile(color='Y', turns_left=6)
@@ -42,28 +42,28 @@ TILE_MATCH_TESTS = [
     (YELLOW, RED, False),
 
     # strike colors
-    (YELLOW, YELLOW_STRIKE_123_LEFT, True),
-    (YELLOW_STRIKE_123_LEFT, RED, False),
-    (YELLOW_STRIKE_123_LEFT, YELLOW_STRIKE_124_LEFT, True),
-    (YELLOW_STRIKE_123_LEFT, YELLOW_STRIKE_123_LEFT, True),
-    (YELLOW_STRIKE_123_LEFT, RED_STRIKE_123_LEFT, False),
+    (YELLOW, YELLOW_STRIKE_123_OFFENSE, True),
+    (YELLOW_STRIKE_123_OFFENSE, RED, False),
+    (YELLOW_STRIKE_123_OFFENSE, YELLOW_STRIKE_124_OFFENSE, True),
+    (YELLOW_STRIKE_123_OFFENSE, YELLOW_STRIKE_123_OFFENSE, True),
+    (YELLOW_STRIKE_123_OFFENSE, RED_STRIKE_123_OFFENSE, False),
 
     # attack colors
-    (YELLOW, YELLOW_ATTACK_123_LEFT, True),
-    (YELLOW_ATTACK_123_LEFT, RED, False),
-    (YELLOW_ATTACK_123_LEFT, YELLOW_ATTACK_124_LEFT, True),
-    (YELLOW_ATTACK_123_LEFT, YELLOW_ATTACK_123_RIGHT, True),
-    (YELLOW_ATTACK_123_LEFT, RED_ATTACK_123_LEFT, False),
-    (YELLOW_STRIKE_124_LEFT, YELLOW_ATTACK_123_RIGHT, True),
+    (YELLOW, YELLOW_ATTACK_123_OFFENSE, True),
+    (YELLOW_ATTACK_123_OFFENSE, RED, False),
+    (YELLOW_ATTACK_123_OFFENSE, YELLOW_ATTACK_124_OFFENSE, True),
+    (YELLOW_ATTACK_123_OFFENSE, YELLOW_ATTACK_123_DEFENSE, True),
+    (YELLOW_ATTACK_123_OFFENSE, RED_ATTACK_123_OFFENSE, False),
+    (YELLOW_STRIKE_124_OFFENSE, YELLOW_ATTACK_123_DEFENSE, True),
 
     # protect colors
-    (YELLOW, YELLOW_PROTECT_123_LEFT, True),
-    (YELLOW_PROTECT_123_LEFT, RED, False),
-    (YELLOW_PROTECT_123_LEFT, YELLOW_PROTECT_124_LEFT, True),
-    (YELLOW_PROTECT_123_LEFT, YELLOW_PROTECT_123_RIGHT, True),
-    (YELLOW_PROTECT_123_LEFT, RED_PROTECT_123_LEFT, False),
-    (YELLOW_STRIKE_124_LEFT, YELLOW_PROTECT_123_RIGHT, True),
-    (YELLOW_ATTACK_124_LEFT, YELLOW_PROTECT_123_RIGHT, True),
+    (YELLOW, YELLOW_PROTECT_123_OFFENSE, True),
+    (YELLOW_PROTECT_123_OFFENSE, RED, False),
+    (YELLOW_PROTECT_123_OFFENSE, YELLOW_PROTECT_124_OFFENSE, True),
+    (YELLOW_PROTECT_123_OFFENSE, YELLOW_PROTECT_123_DEFENSE, True),
+    (YELLOW_PROTECT_123_OFFENSE, RED_PROTECT_123_OFFENSE, False),
+    (YELLOW_STRIKE_124_OFFENSE, YELLOW_PROTECT_123_DEFENSE, True),
+    (YELLOW_ATTACK_124_OFFENSE, YELLOW_PROTECT_123_DEFENSE, True),
 
     # countdown colors
     (YELLOW, YELLOW_COUNTDOWN_5, True),
@@ -71,17 +71,17 @@ TILE_MATCH_TESTS = [
     (YELLOW_COUNTDOWN_5, YELLOW_COUNTDOWN_6, True),
     (YELLOW_COUNTDOWN_5, YELLOW_COUNTDOWN_5, True),
     (YELLOW_COUNTDOWN_5, RED_COUNTDOWN_5, False),
-    (YELLOW_STRIKE_123_LEFT, YELLOW_COUNTDOWN_5, True),
-    (YELLOW_ATTACK_123_LEFT, YELLOW_COUNTDOWN_5, True),
-    (YELLOW_PROTECT_123_LEFT, YELLOW_COUNTDOWN_5, True),
+    (YELLOW_STRIKE_123_OFFENSE, YELLOW_COUNTDOWN_5, True),
+    (YELLOW_ATTACK_123_OFFENSE, YELLOW_COUNTDOWN_5, True),
+    (YELLOW_PROTECT_123_OFFENSE, YELLOW_COUNTDOWN_5, True),
 
     # empty tiles
     (EMPTY, EMPTY, False),
     (EMPTY, EMPTY2, False),
     (EMPTY, YELLOW, False),
-    (EMPTY, YELLOW_STRIKE_123_LEFT, False),
-    (EMPTY, YELLOW_ATTACK_123_LEFT, False),
-    (EMPTY, YELLOW_PROTECT_123_LEFT, False),
+    (EMPTY, YELLOW_STRIKE_123_OFFENSE, False),
+    (EMPTY, YELLOW_ATTACK_123_OFFENSE, False),
+    (EMPTY, YELLOW_PROTECT_123_OFFENSE, False),
     (EMPTY, YELLOW_COUNTDOWN_5, False),
     (EMPTY, CRITICAL, False),
     (EMPTY, TEAMUP, False),
@@ -92,9 +92,9 @@ TILE_MATCH_TESTS = [
     (CRITICAL, YELLOW, True),
     (CRITICAL, RED, True),
     (CRITICAL, YELLOW_COUNTDOWN_6, True),
-    (CRITICAL, YELLOW_STRIKE_123_LEFT, True),
-    (CRITICAL, YELLOW_ATTACK_123_LEFT, True),
-    (CRITICAL, YELLOW_PROTECT_123_LEFT, True),
+    (CRITICAL, YELLOW_STRIKE_123_OFFENSE, True),
+    (CRITICAL, YELLOW_ATTACK_123_OFFENSE, True),
+    (CRITICAL, YELLOW_PROTECT_123_OFFENSE, True),
 
     # teamup
     (TEAMUP, TEAMUP, True),
@@ -103,9 +103,9 @@ TILE_MATCH_TESTS = [
     (TEAMUP, YELLOW, False),
     (TEAMUP, RED, False),
     (TEAMUP, YELLOW_COUNTDOWN_6, False),
-    (TEAMUP, YELLOW_STRIKE_123_LEFT, False),
-    (TEAMUP, YELLOW_ATTACK_123_LEFT, False),
-    (TEAMUP, YELLOW_PROTECT_123_LEFT, False),
+    (TEAMUP, YELLOW_STRIKE_123_OFFENSE, False),
+    (TEAMUP, YELLOW_ATTACK_123_OFFENSE, False),
+    (TEAMUP, YELLOW_PROTECT_123_OFFENSE, False),
 ]
 
 
