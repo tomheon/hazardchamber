f11dbfa6b9644360888501ef4f399aa8e9196f46
Edmund Jorgensen <tomheon@gmail.com>
2015-05-14 23:27:28 -0400

    Add ap-seeking strategy.

--- strategy.py
+++ strategy.py
@@ -3,12 +3,17 @@ Various simple strategies.
 """
 
 from collections import namedtuple
+import itertools
 import random
 
 from board import neighbors
 import board_aware_cache
+from criticals import calc_critical_square
+from gravity import apply_gravity
 from match import find_matches
 from square import sq
+from tile_destroyer import destroy_tiles
+from tiles import CriticalTile
 
 
 MoveWithMatches = namedtuple('MoveWithMatches',
@@ -88,3 +93,60 @@ def no_move_strat(game_state):
     Refuse to move, a la 3 minion teams.
     """
     return None
+
+
+def _ap(from_sq, to_sq, matches, colors, board):
+    """
+    Return a list, the same length as `colors`, with each element being the
+    number of ap in that color earned by `match`.
+    """
+    ap = [0] * len(colors)
+
+    # later, do prob. ev of tiles that fall
+
+    board = board.copy()
+    board.swap(from_sq[0], from_sq[1], to_sq[0], to_sq[1])
+
+    while find_matches(board):
+        new_board, destroyed_sqs = destroy_tiles(board)
+        for s in destroyed_sqs:
+            sq_ap = board.at(s[0], s[1]).ap()
+            if sq_ap and sq_ap[0] in colors:
+                ap[colors.index(sq_ap[0])] += sq_ap[1]
+        for match in matches:
+            crit = calc_critical_square(match)
+            if crit:
+                new_board.set_at(crit[0], crit[1], CriticalTile())
+        board, _ = apply_gravity(new_board)
+
+    return ap
+
+
+def ap_seeking_strat(colors):
+    """
+    Return a strategy that seeks AP in `colors`, preferring those earlier in
+    `colors` to those later.
+
+    Will select the match that generates the most AP in the most preferred
+    color, breaking any ties with reference to the next most preferred etc.
+
+    Ties are broken with reference to the lexically earliest swapped tiles.
+    """
+    colors = list(colors)
+
+    def _ap_seeking_strat(game_state):
+        moves_with_matches = find_moves(game_state.board)
+        ap_plus_move = [(_ap(mwm.to_sq,
+                             mwm.from_sq,
+                             mwm.matches,
+                             colors,
+                             game_state.board),
+                         (mwm.to_sq, mwm.from_sq))
+                        for mwm
+                        in moves_with_matches]
+        ap_plus_move.sort(reverse=True)
+        for _, moves in itertools.groupby(ap_plus_move, lambda t: t[0]):
+            return sorted(moves)[0][1]
+        return None
+
+    return _ap_seeking_strat
--- tests/test_strategy.py
+++ tests/test_strategy.py
@@ -6,10 +6,11 @@ from game import GameState
 from match import Match
 from parse import create_board_parser, parse_board
 from strategy import find_moves, rand_move_strat, first_move_strat, \
-    no_move_strat
+    no_move_strat, ap_seeking_strat
 from tutils import right_match, down_match
 
 FOUR_SIDE_PARSER = create_board_parser(side=4)
+FIVE_SIDE_PARSER = create_board_parser(side=5)
 
 # (board_s, parser, exp_moves_non_sym)
 FIND_MOVES_CASES = [
@@ -135,6 +136,124 @@ def test_no_move_strat():
 
 def _verify_no_move_strat(board):
     eq_(None, no_move_strat(_game_state(board)))
+
+
+# (desc, board, parser, colors, exp move)
+AP_SEEKING_CASES = [
+    ("Handles no available moves",
+     dedent("""\
+            | Y | P | P | Y |
+            | R | G | R | Y |
+            | Y | G | R | G |
+            | R | Y | P | G |
+            """),
+     FOUR_SIDE_PARSER,
+     ['Y'],
+     None),
+
+    ("Chooses first preferred color",
+     dedent("""\
+            |   | P | P |   |
+            | P | G |   |   |
+            |   | G |   |   |
+            | G |   |   |   |
+            """),
+     FOUR_SIDE_PARSER,
+     ['P', 'G'],
+     ((0, 0), (1, 0))),
+
+    ("Chooses first preferred color take 2",
+     dedent("""\
+            |   | P | P |   |
+            | P | G |   |   |
+            |   | G |   |   |
+            | G |   |   |   |
+            """),
+     FOUR_SIDE_PARSER,
+     ['G', 'P'],
+     ((3, 0), (3, 1))),
+
+    ("Chooses first preferred color take 3",
+     dedent("""\
+            |   | P | P |   |
+            | P | G |   |   |
+            |   | G |   |   |
+            | G |   |   |   |
+            """),
+     FOUR_SIDE_PARSER,
+     ['G'],
+     ((3, 0), (3, 1))),
+
+    ("When no preferred available, chooses lexically first",
+     dedent("""\
+            |   | P | P |   |
+            | P | G |   |   |
+            |   | G |   |   |
+            | G |   |   |   |
+            """),
+     FOUR_SIDE_PARSER,
+     ['Y'],
+     ((0, 0), (1, 0))),
+
+    ("Includes destroyed tiles in calculations",
+     dedent("""\
+            | Y |   | Y | Y |   |
+            |   | Y |   |   |   |
+            | Y |   | Y | Y | G |
+            |   |   |   |   |   |
+            |   |   |   |   |   |
+            """),
+     FIVE_SIDE_PARSER,
+     ['Y', 'G'],
+     ((1, 1), (2, 1))),
+
+    ("Includes post-gravity matches in calculations",
+     dedent("""\
+            |   |   | Y | Y |   |
+            |   | Y |   |   |   |
+            |   |   | P |   |   |
+            |   | P |   |   |   |
+            | Y | P | Y |   |   |
+            """),
+     FIVE_SIDE_PARSER,
+     ['Y', 'P'],
+     ((2, 1), (2, 2))),
+
+    ("Includes post-gravity crits in calculations",
+     dedent("""\
+            |   | Y |   | Y |   |
+            |   | Y |   | Y |   |
+            |   |   | Y |   |   |
+            |   | Y |   | Y |   |
+            | P | Y | P | Y |   |
+            """),
+     FIVE_SIDE_PARSER,
+     ['Y', 'P'],
+     ((2, 1), (2, 2))),
+
+    ("Includes post-gravity crits in calculations 2",
+     dedent("""\
+            |   | Y |   | Y |   |
+            |   | Y |   | Y |   |
+            |   |   | Y |   |   |
+            |   | Y |   | Y |   |
+            |   | Y | P | Y | P |
+            """),
+     FIVE_SIDE_PARSER,
+     ['Y', 'P'],
+     ((2, 2), (2, 3))),
+
+    ]
+
+
+def test_ap_seeking_strat():
+    for desc, board_s, parser, colors, exp_move in AP_SEEKING_CASES:
+        board = parse_board(board_s, parser)
+        yield _verify_ap_seeking_strat, desc, board, colors, exp_move
+
+
+def _verify_ap_seeking_strat(desc, board, colors, exp_move):
+    eq_(exp_move, ap_seeking_strat(colors)(_game_state(board)))
 
 
 def _game_state(board):
